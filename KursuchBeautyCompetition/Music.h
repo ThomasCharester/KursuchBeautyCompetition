1288979cd532bc4e186b19a5312304d5eb505d58
ThomasCharester <thomascharester@gmail.com>
2024-05-10 14:33:12 +0300

    A

--- KursuchBeautyCompetition/Music.h
+++ KursuchBeautyCompetition/Music.h
@@ -3,6 +3,6 @@
 #pragma comment (lib, "winmm.lib")
 
 void startPlaying() {
-	PlaySound(TEXT("Default_Music_Menu_1.wav"), NULL, SND_FILENAME | SND_ASYNC);
+	PlaySound(TEXT("Default_Music_Menu_1.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
 
 }
